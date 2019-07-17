ebd4dc26a27018e44af6516da5e45e80e7c2a54a
Rajiv Mayani <mayani@isi.edu>
2019-07-16 17:45:56 -0700

    Add decorators to process DAX transformations

--- src/pegasus_cycles/_pegasus.py
+++ src/pegasus_cycles/_pegasus.py
@@ -20,18 +20,45 @@ class ADAG(Base):
 
         return wrap
 
+    def transformation(self):
+        def wrap(f):
+            @wraps(f)
+            def wrapped_f(*args, **kwargs):
+                rv = f(*args, **kwargs)
+                if rv:
+                    rv = rv if isinstance(rv, (list, tuple)) else [rv]
+                    t = Transformation(rv[0].name)
+                    for e in rv:
+                        t.uses(e)
+                        try:
+                            if isinstance(e, Executable):
+                                self.addExecutable(e)
+                            else:
+                                self.addFile(e)
+                        except DuplicateError:
+                            pass
+
+                    self.addTransformation(t)
+
+                return rv
+
+            return wrapped_f
+
+        return wrap
+
     def resource_info(self, cpu=1, core=1, memory=1):
         def wrap(f):
             @wraps(f)
             def wrapped_f(*args, **kwargs):
                 rv = f(*args, **kwargs)
                 if rv:
+                    _rv = rv[0] if isinstance(rv, (list, tuple)) else rv
                     if cpu != 1:
-                        rv.profile(Namespace.PEGASUS, "cpu", cpu)
+                        _rv.profile(Namespace.PEGASUS, "cpu", cpu)
                     if core != 1:
-                        rv.profile(Namespace.PEGASUS, "core", core)
+                        _rv.profile(Namespace.PEGASUS, "core", core)
                     if memory != 1:
-                        rv.profile(Namespace.PEGASUS, "memory", memory)
+                        _rv.profile(Namespace.PEGASUS, "memory", memory)
 
                 return rv
 
