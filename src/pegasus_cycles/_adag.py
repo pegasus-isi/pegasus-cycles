10bb33efeb5f8b8ff24b85c03b71c3d3c9e5f575
Rafael Ferreira da Silva <rafsilva@isi.edu>
2019-08-05 14:57:57 -0700

    unescaping URL

--- src/pegasus_cycles/_adag.py
+++ src/pegasus_cycles/_adag.py
@@ -35,7 +35,7 @@ def gldas_to_cycles(
 ):
     """Transform GLDAS to Cycles."""
     j = Job("gldas-to-cycles")
-    j.addProfile(Profile(Namespace.CONDOR, key="+SingularityImage", value=html.unescape("&quot;/cvmfs/singularity.opensciencegrid.org/mintproject/cycles:0.9.4-alpha\&quot;")))
+    j.addProfile(Profile(Namespace.CONDOR, key="+SingularityImage", value=html.unescape("&quot;/cvmfs/singularity.opensciencegrid.org/mintproject/cycles:0.9.4-alpha&quot;")))
     j.addArguments("--start-date", start_date)
     j.addArguments("--end-date", end_date)
     j.addArguments("--latitude", latitude)
@@ -83,7 +83,7 @@ def cycles(
     """Cycles."""
     prefix = "baseline_" if baseline else "fertilizer_increase_" if fertilizer_increase else ""
     j = Job(prefix + "cycles")
-    j.addProfile(Profile(Namespace.CONDOR, key="+SingularityImage", value=html.unescape("&quot;/cvmfs/singularity.opensciencegrid.org/mintproject/cycles:0.9.4-alpha\&quot;")))
+    j.addProfile(Profile(Namespace.CONDOR, key="+SingularityImage", value=html.unescape("&quot;/cvmfs/singularity.opensciencegrid.org/mintproject/cycles:0.9.4-alpha&quot;")))
     j.addArguments("--baseline", str(baseline))
     j.addArguments("--fertilizer-increase", str(fertilizer_increase))
     j.addArguments("--id", unique_id)
